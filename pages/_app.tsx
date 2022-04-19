bd4da314b5c654d44bca587feed336b701a8bbf3
byung ju choi <bjc30849@gmail.com>
2022-04-19 19:58:53 +0900

    global styled added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
-import Layout from "../components/Layout"
-import type { AppProps } from "next/app"
+import Head from 'next/head'
+import Layout from '../components/Layout'
+import type { AppProps } from 'next/app'
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>boilerplate</title>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 //BEM 방식 ( block , element , model )
 
 const index = () => {
-  return <div>index</div>
+  return <Wrapper>index</Wrapper>
 }
+
+const Wrapper = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 export default index
