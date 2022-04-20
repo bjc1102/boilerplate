65a2edec2b0351b818726035060362742b3bd06b
byung ju choi <bjc30849@gmail.com>
2022-04-20 13:36:03 +0900

    global style added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Layout from '../components/Layout'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { Theme } from '../styles/theme'
+import { GlobalStyle } from '../styles/global-style'
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,7 @@ export default function CustomApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>boilerplate</title>
       </Head>
+      <GlobalStyle />
       <ThemeProvider theme={Theme}>
         <Layout>
           <Component {...pageProps} />
